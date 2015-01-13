1453e571a11889e872ccf2664b0dad7d69ed81db
kastesh <k.sh770@gmail.com>
2015-01-13 10:06:04 +0200

    nginx script to common structure

--- scripts/nginx_stat.sh
+++ scripts/nginx_stat.sh
@@ -1,65 +1,105 @@
 #!/bin/bash
- 
-### Автор скрипта - sirkonst@gmail.com
-### Сайт поддержки - http://wiki.enchtex.info/howto/zabbix/nginx_monitoring
- 
-### DESCRIPTION
-# $1 - имя узла сети в zabbix'е (не используется)
-# $2 - измеряемая метрика
-# $3 - http ссылка на станицу статистику nginx
- 
-### OPTIONS VERIFICATION
-if [[ -z "$1" || -z "$2" ]]; then
-    exit 1
-fi
- 
-### PARAMETERS
-METRIC="$1"  # измеряемая метрика
-STATURL="$2" # адрес nginx статистики
- 
-CURL="/usr/bin/curl -k"
- 
-CACHETTL="55" # Время действия кеша в секундах (чуть меньше чем период опроса элементов)
-CACHE="/tmp/nginxstat-`echo $STATURL | md5sum | cut -d" " -f1`.cache"
- 
-### RUN
- 
-## Проверка кеша:
-# время создание кеша (или 0 есть файл кеша отсутствует или имеет нулевой размер)
-if [ -s "$CACHE" ]; then
-    TIMECACHE=`stat -c"%Z" "$CACHE"`
-else
-    TIMECACHE=0
-fi
-# текущее время
-TIMENOW=`date '+%s'`
-# Если кеш неактуален, то обновить его (выход при ошибке)
-if [ "$(($TIMENOW - $TIMECACHE))" -gt "$CACHETTL" ]; then
-    $CURL -s "$STATURL" > $CACHE || exit 1
-fi
- 
- 
-## Извлечение метрики:
- 
-if [ "$METRIC" = "active" ]; then
-    cat $CACHE | grep "Active connections" | cut -d':' -f2
-fi
-if [ "$METRIC" = "accepts" ]; then
-    cat $CACHE | sed -n '3p' | cut -d" " -f2
-fi
-if [ "$METRIC" = "handled" ]; then
-    cat $CACHE | sed -n '3p' | cut -d" " -f3
-fi
-if [ "$METRIC" = "requests" ]; then
-    cat $CACHE | sed -n '3p' | cut -d" " -f4
-fi
-if [ "$METRIC" = "reading" ]; then
-    cat $CACHE | grep "Reading" | cut -d':' -f2 | cut -d' ' -f2
-fi
-if [ "$METRIC" = "writing" ]; then
-    cat $CACHE | grep "Writing" | cut -d':' -f3 | cut -d' ' -f2
-fi
-if [ "$METRIC" = "waiting" ]; then
-    cat $CACHE | grep "Waiting" | cut -d':' -f4 | cut -d' ' -f2
-fi
+# discovery status for devices for nginx by stab module
+# usage in template via zabbix agent
+# additional info: 
+# http://wiki.enchtex.info/howto/zabbix/nginx_monitoring
+# ex
+export LC_ALL=""
+export LANG="en_US.UTF-8"
+export PATH=$PATH:/sbin:/usr/sbin:$HOME/bin
+
+PROGNAME=$(basename $0)
+PROGPATH=$(dirname $0)
+VERBOSE=0
+TEST=$(echo $PROGNAME| sed -e 's/\.sh$//')
+CACHESEC=55
+DATE_TM=$(date +%s)
+
+TMPDIR=/dev/shm
+[[ ! -d $TMPDIR ]] && TMPDIR=/tmp
+
+WORKDIR=/home/zabbix
+[[ ! -d $WORKDIR ]] && WORKDIR=/var/lib/zabbix/local
+
+LOGDIR=$WORKDIR/log
+[[ ! -d $LOGDIR ]]  && mkdir $LOGDIR
+LOGFILE=$LOGDIR/$TEST.log
+TMPFILE=$TMPDIR/${TEST}
+DEFAULT_URL="http://127.0.0.1/nginx_status"
+
+CURLCMD="/usr/bin/curl -k"
+PGREP="sudo /usr/bin/pgrep -x"
+
+. $PROGPATH/zabbix_utils.sh || exit 1
+
+print_usage(){
+  code=$1
+  echo "Usage: $PROGNAME [-hv] -m active|accepts|handled|requests|reading|writing|waiting|process -u url"
+  echo " $PROGNAME -m active -u http://127.0.0.1:8080/nginx_status"
+  echo
+  exit $code
+}
+
+nginx_status(){
+  url=$1
+  metric=$2
+
+  [[ ( -z $metric ) || ( -z $url ) ]] && exit 1
+
+  metric_cache=${TMPFILE}_$(echo "$url" | md5sum | awk '{print $1}')
+  metric_ttl=56
+
+  # test if cache file is valid
+  use_cache=$(test_cache $metric_cache $metric_ttl)
+
+  # create cache
+  if [[ $use_cache -eq 1 ]]; then
+    $CURLCMD -s "$url" >$metric_cache 2>&1
+    if [[ $? -gt 0 ]]; then
+      print_debug "cmd=\`$CURLCMD -s \"$url\"\` return error: $(head -1 $metric_cache)"
+      rm -f $metric_cache
+      exit 1
+    fi
+  fi
+
+  # use cache
+  case "$metric" in
+    "active")   cat $metric_cache | grep "Active connections" | cut -d':' -f2 ;;
+    "accepts")  cat $metric_cache | sed -n '3p' | cut -d" " -f2 ;;
+    "handled")  cat $metric_cache | sed -n '3p' | cut -d" " -f3 ;;
+    "requests") cat $metric_cache | sed -n '3p' | cut -d" " -f4 ;;
+    "reading")  cat $metric_cache | grep "Reading" | cut -d':' -f2 | cut -d' ' -f2 ;;
+    "writing")  cat $metric_cache | grep "Writing" | cut -d':' -f2 | cut -d' ' -f2 ;;
+    "waiting")  cat $metric_cache | grep "Waiting" | cut -d':' -f2 | cut -d' ' -f2 ;;
+    "process")  $PGREP nginx | wc -l ;;
+    *)
+      print_debug "Cannot use metric=$metric"
+      exit
+    ;;
+  esac
+}
+
+while getopts ":m:u:vh" opt; do
+  case $opt in
+    "m")
+      METRIC=$OPTARG          # requested metric
+      ;;
+    "u")
+      URL=$OPTARG             # http url with nginx stab info
+      ;;
+    h)
+      print_usage 0
+      ;;
+    v)
+      VERBOSE=1
+      ;;
+    \?)
+      print_usage 1
+      ;;
+  esac
+done
+
+[[ -z "$URL" ]] && URL=$DEFAULT_URL
+
+nginx_status "$URL" "$METRIC"
 
